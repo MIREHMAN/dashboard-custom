852510332973b7654fceed0efe7b651a3f93d6ff
MIREHMAN <Inammalik73@gmail.com>
2024-10-24 21:43:49 +0500

    Dashboard Design

---
+++ src/components/AdminDashboard.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+
+import { alpha } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import AppNavbar from './DashboardComponents/AppNavbar';
+import Header from './DashboardComponents/Header';
+import SideMenu from './DashboardComponents/SideMenu';
+import DashboardContent from './DashboardComponents/DashboardContent';
+
+
+export default function AdminDashboard(props) {
+  return (
+    
+      <Box sx={{ display: 'flex' }}>
+        <SideMenu />
+        <AppNavbar />
+        {/* Main content */}
+        <Box
+          component="main"
+          sx={(theme) => ({
+            flexGrow: 1,
+            backgroundColor: theme.vars
+              ? `rgba(${theme.vars.palette.background.defaultChannel} / 1)`
+              : alpha(theme.palette.background.default, 1),
+            overflow: 'auto',
+          })}
+        >
+          <Stack
+            spacing={2}
+            sx={{
+              alignItems: 'center',
+              mx: 3,
+              pb: 10,
+              mt: { xs: 8, md: 0 },
+            }}
+          >
+            <Header />
+            
+          </Stack>
+        </Box>
+      </Box>
+   
+  );
+}
